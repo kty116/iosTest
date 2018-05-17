844c6ba3b813497c2e7c43b7c29fa9970e5aa09f
kyoungaeKim <kyoungae3017@gmail.com>
2018-05-17 10:48:07 +0900

    첫번째 ios 커밋

---
+++ Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var languages = ["java","swift"]
+var caps = ["한국": "서울","북한": "평양"]
+
+for language in languages{
+    print("저는 \(language) 언어를 다룰 수 있습니다.")
+}
+
+for i in 0..<10 {
+    print(i)
+}
+
+
